eca8677fa6c544b83387795d8ddd72ba442d1f53
Gizel Liang <gizelliang@gmail.com>
2023-06-07 17:22:30 +0800

    Finish the layout for the home page

---
+++ js/all.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    // 開起選單
+$('.dropdown-btn').on("click",function(e) {
+    $('.dropdown-menu').toggleClass('show');
+  });
+  
+  // 切換按鈕文字
+  $('.new-to-old').on("click",function(e) {
+    e.preventDefault();
+    $('.dropdown-menu').toggleClass('show');
+   $('.dropdown-btnText').text($('.new-to-old').text());
+  });
+  
+  $('.old-to-new').on("click",function(e) {
+    e.preventDefault();
+    $('.dropdown-menu').toggleClass('show');
+    $('.dropdown-btnText').text($('.old-to-new').text());
+  });
+  
+  // .text() 運用：https://www.w3schools.com/jquery/html_text.asp
+
+})
